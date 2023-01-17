f8ce0af5e32ac1511df418ae46faf7efef328c64
code_for_vish <vishalpatil550@gmail.com>
2023-01-17 16:01:37 +0530

    Update config.js

--- Backend/config.js
+++ Backend/config.js
@@ -1,12 +1,12 @@
 export const GOOGLE_CLIENT_ID =
   process.env.GOOGLE_CLIENT_ID ||
-  "611316697650-9hjo3taie8ha957dh36a3f27cca0qfvp.apps.googleusercontent.com";
+  ".apps.googleusercontent.com";
 export const GOOGLE_CLIENT_SECRET =
-  process.env.GOOGLE_CLIENT_SECRET || "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";
+  process.env.GOOGLE_CLIENT_SECRET || "G";
 export const SERVER_ROOT_URI = "http://localhost:4000";
 export const JWT_ACCESS_TOKEN =
   process.env.ACCESS_TOKEN ||
-  "14bffb2fa4bfcb82859aa52402e96088ec9ba9f0d89c02ea320c4d8f40a9157c0686d5fcae2aebddc03f5d54764babfe8246137a546f781bff9409caada7ca5b";
+  "14bffb2fa4bfcb82859aa52402e96088ec9ba9f0d89c02";
 export const JWT_REFRESH_TOKEN =
   process.env.ACCESS_TOKEN || "refreshtoken-jwt-for-ngo";
 
